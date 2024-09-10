ef4b051db10ad241133855b9c00306bb537dde38
RialinArdians <169133349+RialinArdians@users.noreply.github.com>
2024-09-10 20:39:40 +0700

    Add files via upload
    Tugas Pemrograman Mahir

---
+++ Profil.java
@@ -0,0 +1,19 @@
+public class Profil {
+    public static void main(String[] args) {
+        String Nama ;
+        String Nim ;
+        String Kelas ;
+        String Email ;
+
+        Nama = "Rialin Ardiansah";
+        Nim = "12350113034";
+        Kelas = "D";
+        Email =  "rialinardiansah19122004";
+
+        System.out.println("Nama    :"+Nama);
+        System.out.println("Nim     :"+Nim);
+        System.out.println("Kelas   :"+Kelas);
+        System.out.println("Email   :"+Email);
+
+    }
+}
